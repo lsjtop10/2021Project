0a54dd2750f2a0862ba61229d92697857f05dd0c
lsjtop10 <lsjtop10@gmail.com>
2021-12-30 21:35:41 +0900

    초기 커밋.

---
+++ 2021Project/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2021Project
+{
+    
+    class Breeder
+    {
+
+    }
+
+    class Cell
+    {
+
+        //염색체
+
+        //감수분열
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
